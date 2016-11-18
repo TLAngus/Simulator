7dd2cc221b8715096be4adb924dd5da3a8ed8fbe
gillesbraun <braun.gillo@gmail.com>
2016-11-18 13:00:28 +0100

    removed debug messages

--- SimulatorModel/src/model/entities/Hitman.java
+++ SimulatorModel/src/model/entities/Hitman.java
@@ -27,37 +27,29 @@ public class Hitman extends Entity {
             if(x > 0 && y > 0) { // move down right
                 c+=1;
                 r+=1;
-                System.out.println("down right");
             } else if(x > 0 && y < 0) { // move up right
                 c+=1;
                 r-=1;
-                System.out.println("up right");
             } else if(x < 0 && y > 0) { // move down left
                 r+=1;
                 c-=1;
-                System.out.println("down left");
             } else if(x < 0 && y < 0) { // move up left
                 r-=1;
                 c-=1;
-                System.out.println("up left");
             } else {
                 if(x == 0) {
                     if(y > 0) { // straight down
                         r+=2;
-                        System.out.println("staright down");
-                    } else {
+                    } else { // up
                         r-=2;
-                        System.out.println("staright up");
                     }
                 } 
                 
                 if(y == 0) {
                     if(x > 0) { // straight right
                         c+=2;
-                        System.out.println("staright right");
-                    } else {
+                    } else { // left
                         c-=2;
-                        System.out.println("staright left");
                     }
                 }
                 
